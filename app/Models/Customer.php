551737ec6a770fca98ef0875f86bad3ec71ab138
solomon-ctg <solomon.iroegbu@gmail.com>
2024-02-06 17:13:34 +0100

    add Customers by Stage: Tabs with Numbers

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -41,5 +42,22 @@ class Customer extends Model
     public function tags(): BelongsToMany
     {
         return $this->belongsToMany(Tag::class);
+    }
+
+    // relationship
+    public function pipelineStageLogs(): HasMany
+    {
+        return $this->hasMany(CustomerPipelineStage::class);
+    }
+
+    //The last thing to do is to create the Observers to log all the Pipeline Stage changes:
+    public static function booted(): void
+    {
+        self::created(function (Customer $customer) {
+            $customer->pipelineStageLogs()->create([
+                'pipeline_stage_id' => $customer->pipeline_stage_id,
+                'user_id' => auth()->check() ? auth()->id() : null
+            ]);
+        });
     }
 }
